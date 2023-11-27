3fb82d6bd319ad0dd103eb04b89e66c8dc2eea21
Isabelli Marques <101362239+isabellimrqs@users.noreply.github.com>
2023-11-27 11:16:35 -0300

    Add files via upload

---
+++ Initial.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Initial extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
