772d3a4d58f65707065abdcc1d9712e9ff0f7bbd
Isabelli Marques <101362239+isabellimrqs@users.noreply.github.com>
2023-11-27 11:29:16 -0300

    Add files via upload

--- JFXLogin.java
+++ JFXLogin.java
@@ -1,4 +1,18 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class JFXLogin {
+    public TextField txtUsuario;
+    public TextField txtSenha;
+    public Button btnEntrar;
+    public Button btnFinalizar;
+
+    public void login(ActionEvent actionEvent) {
+    }
+
+    public void logout(ActionEvent actionEvent) {
+    }
 }
